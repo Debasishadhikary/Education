d8e2431d903d1db445b01151348abc7ced49917d
Debasishadhikary <sapnadishagroup@gmail.com>
2023-02-22 13:36:17 +0530

    Website

---
+++ script.js
@@ -0,0 +1,11 @@
+function menucall(){
+   const elem = 
+   document.getElementById("menubar");
+   elem.style.display = "block";
+}
+function menuclose(){
+    const elem1 = 
+    document.getElementById("menubar");
+    elem1.style.display = "none";
+ }
+ 
